6602306239ba5d01db54d4bf6c2a794061f8fe7d
mudit4444 <muditbajpai@hotmail.com>
2019-11-06 23:33:42 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {ProductListComponent } from './products/product-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component'; 
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent , 
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
